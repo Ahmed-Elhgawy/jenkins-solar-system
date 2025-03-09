1d1daaec6ffe587a8722c13089dd0496170885e1
Ahmed Elhgawy <ahmadinejad.am50@gmail.com>
2025-03-09 23:16:32 +0200

    bash edit

--- integration-testing-ec2.sh
+++ integration-testing-ec2.sh
@@ -5,13 +5,14 @@ echo "Integration Testing ..."
 aws --version
 Data=$(aws ec2 describe-instances)
 URL=$(aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | select(.Tags[].Value == "Docker server") | .PublicDnsName')
+echo "URL: $URL"
 
 if [[ "$URL" != '' ]]; then
     http_code=$(curl -s -o /dev/null -w "%{http_code}" http://$URL:3000/live)
     if [[ "$http_code" == 200 ]]; then
         echo "Integration Testing Passed"
     else
-        echo "Integration Testing Failed"
+        echo "Integration Testing Failed, liveness probe failed"
         exit 1;
     fi;
 else
