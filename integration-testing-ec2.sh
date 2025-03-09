4d0f7a7e85ae64c8159f8c9d40f84787c9330bfa
Ahmed Elhgawy <ahmadinejad.am50@gmail.com>
2025-03-09 23:18:14 +0200

    fix app port

--- integration-testing-ec2.sh
+++ integration-testing-ec2.sh
@@ -8,7 +8,7 @@ URL=$(aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | select(.
 echo "URL: $URL"
 
 if [[ "$URL" != '' ]]; then
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" http://$URL:3000/live)
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" http://$URL:5000/live)
     if [[ "$http_code" == 200 ]]; then
         echo "Integration Testing Passed"
     else
